1af0d4b2961e1ee673afc0aeb9ed6e8aaffe2f0f
Ravibhushan Kumar <ravibhushankumar@Ravibhushans-MacBook-Air.local>
2025-01-14 20:46:11 +0530

    Adding new config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  basePath : '/portfolio'
+  trailingSlash : true,
+  output: 'export',
   /* config options here */
 };
 
